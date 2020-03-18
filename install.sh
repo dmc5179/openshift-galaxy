b583855b27e519ef6eb4b291a29a4c2d3f1efbbc
Dan Clark <danielmclark@gmail.com>
2020-03-18 18:48:25 +0000

    Add deployment config, image stream, and service along with updates to installer script for galaxy dev releases

--- install.sh
+++ install.sh
@@ -7,14 +7,8 @@ oc adm policy add-scc-to-user anyuid -z default
 # Create service accounts
 oc apply -f 00-serviceaccount.yaml 
 
-
 # Create image streams
-oc apply -f galaxy-api-imagestream.yaml
-oc apply -f galaxy-celery-beat-imagestream.yaml
-oc apply -f galaxy-celery-worker-imagestream.yaml
-oc apply -f galaxy-pulp-rm-imagestream.yaml
-oc apply -f galaxy-pulp-worker-imagestream.yaml
-oc apply -f galaxy-ui-imagestream.yaml
+oc apply -f galaxy-dev-imagestream.yaml
 oc apply -f influxdb-imagestream.yaml
 oc apply -f postgres-imagestream.yaml
 oc apply -f rabbitmq-imagestream.yaml
@@ -39,35 +33,11 @@ oc apply -f postgres-service.yaml
 oc apply -f rabbitmq-service.yaml
 oc apply -f redis-service.yaml
 
+# Deploy Galaxy
+oc apply -f galaxy-dev-deploymentconfig.yaml
 
-# Deploy the galaxy API
-oc apply -f galaxy-api-deploymentconfig.yaml
-oc rollout status dc/galaxy-api
-oc apply -f galaxy-api-service.yaml
+# Deploy Galaxy Service
+oc apply -f galaxy-dev-imagestream.yaml
 
-
-# Create deployments
-# FAILING doesn't like redis name
-oc apply -f galaxy-pulp-worker-deploymentconfig.yaml
-oc rollout status dc/galaxy-pulp-worker
-
-# FAILING doesn't like redis name
-oc apply -f galaxy-pulp-rm-deploymentconfig.yaml
-oc rollout status dc/galaxy-pulp-rm
-
-# WORKING
-oc apply -f galaxy-celery-worker-deploymentconfig.yaml
-oc rollout status dc/galaxy-celery-worker
-
-# FAILING, who knows why....
-oc apply -f galaxy-celery-beat-deploymentconfig.yaml
-oc rollout status dc/galaxy-celery-beat
-
-oc apply -f galaxy-ui-deploymentconfig.yaml
-oc rollout status dc/galaxy-ui
-
-# Create UI service
-oc apply -f galaxy-ui-service.yaml
-
-# Create UI route
-oc expose svc/galaxy-ui
+# Expose Galaxy
+oc expose svc/galaxy-dev
